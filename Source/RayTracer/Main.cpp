c305a7697c1143966137b849a17bee33e81e5ee3
mckinleyStratton <mstratton@student.neumont.edu>
2023-12-06 23:27:36 -0700

    traingle looks super weird but whatevs

--- Source/RayTracer/Main.cpp
+++ Source/RayTracer/Main.cpp
@@ -33,7 +33,7 @@ int main(int, char**)
     Canvas canvas(width, height, renderer);
     Scene scene(glm::vec3{ 1.0f }, glm::vec3{ 0.5f, 0.7f, 1.0f });
 
-    InitScene02(scene, canvas);
+    InitScene01(scene, canvas);
 
     // render scene
     canvas.Clear({ 0, 0, 0, 1 });
@@ -74,7 +74,30 @@ void InitScene01(Scene& scene, const Canvas& canvas)
 
     scene.SetCamera(camera);
 
-    auto triangle = std::make_unique<Triangle>(glm::vec3{ -1, 0, 0 }, glm::vec3{ 1, 0, 0 }, glm::vec3{ 0, 2, 0 }, std::make_shared<Lambertian>(color3_t{ 1, 0, 0 }));
+    //// Create and add spheres
+    //for (int x = -10; x < 10; x++)
+    //{
+    //    for (int z = -10; z < 10; z++)
+    //    {
+    //        std::shared_ptr<Material> material;
+
+    //        // create random material
+    //        float r = random01();
+    //        if (r < 0.3f)		material = std::make_shared<Lambertian>(glm::rgbColor(glm::vec3{ random(0, 360), 1.0f, 1.0f }));
+    //        else if (r < 0.6f)	material = std::make_shared<Metal>(color3_t{ random(0.5f, 1.0f) }, random(0, 0.5f));
+    //        else if (r < 0.9f)	material = std::make_shared<Dielectric>(color3_t{ 1.0f }, random(1.1f, 2));
+    //        else				material = std::make_shared<Emissive>(glm::rgbColor(glm::vec3{ random(0, 360), 1.0f, 1.0f }), 5.0f);
+
+    //        // set random sphere radius
+    //        float radius = random(0.2f, 0.3f);
+    //        // create sphere using random radius and material
+    //        auto sphere = std::make_unique<Sphere>(glm::vec3{ x + random(-0.5f, 0.5f), radius, z + random(-0.5f, 0.5f) }, radius, material);
+    //        // add sphere to the scene
+    //        scene.AddObject(std::move(sphere));
+    //    }
+    //}
+
+    auto triangle = std::make_unique<Triangle>(glm::vec3{ 1, 1, 20 }, glm::vec3{ -1, -1, -1 }, glm::vec3{ 0, 2, 0 }, std::make_shared<Lambertian>(color3_t{ 0, 0, 1 }));
     scene.AddObject(std::move(triangle));
 
     auto plane = std::make_unique<Plane>(glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, std::make_shared<Lambertian>(color3_t{ 0.2f }));
